cc88cffbe0ca392d7f518aa659984abe463ede6b
Kimberlykwok <183509Q@mymail.nyp.edu.sg>
2020-07-06 21:10:20 +0800

    installed npm

--- routes.js
+++ routes.js
@@ -1,1 +1,14 @@
-//routes
+var bodyParser = require('body-parser');
+
+var routes = function () {
+    var router = require('express').Router();
+    
+    router.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    router.get('/', function(req, res) {
+        res.sendFile(__dirname+"/views/index.html");
+    });
+    return router;
+};
