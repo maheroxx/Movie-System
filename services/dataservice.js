87e79570b1d5b18386c570e412738fb5c7b9d520
Kimberlykwok <183509Q@mymail.nyp.edu.sg>
2020-07-08 13:45:06 +0800

    commit

---
+++ services/dataservice.js
@@ -0,0 +1,9 @@
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose.set('debug',true);
