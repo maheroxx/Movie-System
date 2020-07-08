56344bbbc5a7b1ac36ca4137d5bd66b02785e24b
Kimberlykwok <183509Q@mymail.nyp.edu.sg>
2020-07-08 15:47:36 +0800

    commit

--- services/dataservice.js
+++ services/dataservice.js
@@ -1,19 +1,11 @@
 var mongoose = require('mongoose');
 var schema = mongoose.Schema;
-<<<<<<< HEAD
-=======
-var movieSchema = {};
-var movieModel;
->>>>>>> 95d7c10eeaba320021e04c1610a6856ceb67e9c7
 
 mongoose.set('useNewUrlParser', true);
 mongoose.set('useFindAndModify', false);
 mongoose.set('useCreateIndex', true);
 mongoose.set('useUnifiedTopology', true);
 
-<<<<<<< HEAD
-mongoose.set('debug',true);
-=======
 var database = {
     connect: function () {
         mongoose.connect('mongodb://localhost:27017/MoviesDB', function (err) {
@@ -34,4 +26,3 @@ var database = {
 };
 
 module.exports = database;
->>>>>>> 95d7c10eeaba320021e04c1610a6856ceb67e9c7
